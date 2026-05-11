d5317c32e2c5380850f804f1d9de5387b3da080a
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2026-05-11 21:47:02 +0700

    revert commit 87f50f4f

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,8 +1,58 @@
+export type Ground = {
+  id: number;
+
+  x: number;
+
+  y: number;
+
+  width: number;
+
+  height: number;
+};
+
+export type QuestionBlock = {
+  id: number;
+
+  x: number;
+
+  y: number;
+
+  width: number;
+
+  height: number;
+
+  questionId: number;
+
+  answered: boolean;
+};
+
 export type Question = {
   id: number;
+
   question: string;
+
   choices: string[];
+
   answer: string;
+};
+
+export type Level = {
+  id: number;
+
+  requiredScore: number;
+
+  playerStart: {
+    x: number;
+    y: number;
+  };
+
+  grounds: Ground[];
+
+  questionBlocks: QuestionBlock[];
+
+  worldWidth: number;
+
+  worldHeight: number;
 };
 
 export type Rect = {
@@ -12,11 +62,18 @@ export type Rect = {
   height: number;
 };
 
-export type PlatformType = "normal" | "question" | "energy";
+export type Player = {
+  x: number;
 
-export type Platform = Rect & {
-  id: string;
-  type: PlatformType;
-  qId?: number;
-  used: boolean;
+  y: number;
+
+  width: number;
+
+  height: number;
+
+  velocityY: number;
+
+  speed: number;
+
+  isJumping: boolean;
 };
